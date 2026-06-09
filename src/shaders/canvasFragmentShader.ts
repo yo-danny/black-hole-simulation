c819cf3a76878112557c843331594501e30fd7bb
YoDanny <o.dannymartins@gmail.com>
2026-06-08 21:25:58 -0300

    accretion disk

--- src/shaders/canvasFragmentShader.ts
+++ src/shaders/canvasFragmentShader.ts
@@ -16,6 +16,40 @@ uniform float uPov;
 uniform int uMaxIterations;
 uniform float uStepSize;
 
+// variables
+
+vec3 bh_pos = vec3(0.0, 0.0, 0.0);
+vec3 camera_pos = vec3(0.0, 0.05, 20.0);
+
+float innnerDiskRadius = 2.0;
+float outerDiskRadius = 8.0;
+
+float diskFactor = 3.0;
+float disk_flow = 10.0;
+float flow_rate = 0.6;
+
+// accretion disk
+
+float hash(float n) {
+    return fract(sin(n) * 753.5453123);
+}
+
+float MappingRange(float X, float A, float B, float C, float D) {
+    return (X - A) / (B - A) * (D - C) + C;
+}
+
+float noise(vec3 x){
+    vec3 p = floor(x);
+    vec3 f = fract(x);
+    f = f * f * (3.0 - 2.0 * f);
+    float n = p.x + p.y * 157.0 + 113.0 * p.z;
+
+    return mix(mix(mix(hash(n + 0.0), hash(n + 1.0), f.x),
+            mix(hash(n + 157.0), hash(n + 158.0), f.x), f.y),
+            mix(mix(hash(n + 113.0), hash(n + 114.0), f.x),
+            mix(hash(n + 270.0), hash(n + 271.0), f.x), f.y), f.z);
+}
+
 float fbm(vec3 pos, const int numOctaves, const float iterScale, const float detail, const float weight) {
     float mul = weight;
     float add = 1.0 - 0.5 * mul;
